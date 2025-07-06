00de78b6083d8325221dc543af3f98fef0e1974d
jawa hassan <jawahassan@gmail.com>
2025-07-06 13:36:36 +0300

    api project

--- expense/accounts/urls.py
+++ expense/accounts/urls.py
@@ -5,6 +5,5 @@ from .views import SignUpView,LoginView , VerifyView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', SignUpView.as_view(),name="signup"),
-    path('login/', LoginView.as_view(),name="login"),
-    path('check_verification/', VerifyView.as_view(),name="check_verification"),
+ 
 ]
--- expense/accounts/views.py
+++ expense/accounts/views.py
@@ -19,34 +19,3 @@ class SignUpView(generics.CreateAPIView):
     serializer_class = SingUpSerializer
     permission_classes = [AllowAny]
 
-class LoginView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        
-        if not username or not password:
-            return Response({"error": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
-
-        user = authenticate(username=username, password=password)
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({
-                "message": "Login successful",
-                "token": token.key,
-                "username": user.username
-            }, status=status.HTTP_200_OK)
-        
-        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
-    
-class VerifyView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        if request.user.is_verified:
-            return Response({"message": "your account  is verified. Welcome!"})
-        else:
-            return Response({"message": "please complete verification process"}, status=status.HTTP_403_FORBIDDEN)    
-
-    
